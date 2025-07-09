ab5199619083ffc2f591de925a20c9159a85521c
raquelduda <raquelnegreirosduda@gmail.com>
2025-07-09 14:18:46 -0300

    atualização dropdown

--- principal.js
+++ principal.js
@@ -1,3 +1,15 @@
-function show(){
-    alert("catalogo")
+
+// function show() {
+//     const div = document.getElementById('dropdown');
+//     if (div.style.display === 'none') {
+//         div.style.display = 'flex';
+//     } else {
+//         div.style.display = 'none';
+//     }
+// }
+
+function show() {
+    const div = document.getElementById('dropdown');
+    div.classList.toggle('show');
+    //  document.getElementById('paragrafo').innerHTML = "Categorias ▲"
 }
